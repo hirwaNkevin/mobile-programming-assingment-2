fc17a622392bce2ae03977415abf6e58e01612b5
hirwaNkevin <hirwankevin@gmail.com>
2024-02-19 17:24:01 +0200

    add stack navigation for login and signup

--- App.js
+++ App.js
@@ -14,38 +14,50 @@ import aboutIcon from "./assets/about_icon.svg";
 import { ThemeProvider } from "./components/ThemeProvider";
 import HomeScreenTabedComponent from "./screens/HomeScreen";
 import { StatusBar } from "expo-status-bar";
+import { createStackNavigator } from "@react-navigation/stack";
 
 const Tab = createMaterialBottomTabNavigator();
 const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
   let authenticated = false;
   return authenticated === true ? (
-    <NavigationContainer>
-      <ThemeProvider>
-        <Drawer.Navigator>
-          <Drawer.Screen
-            name="Home"
-            component={HomeScreenTabedComponent}
-            style={{ backgroundColor: "#222" }}
-          ></Drawer.Screen>
-          <Drawer.Screen
-            name="Calculator"
-            component={CalculatorScreenTabedComponent}
-          ></Drawer.Screen>
-          <Drawer.Screen
-            name="About"
-            component={AboutScreenTabedComponent}
-          ></Drawer.Screen>
-        </Drawer.Navigator>
-      </ThemeProvider>
-    </NavigationContainer>
+    <MainApp />
   ) : (
-    <AuthLoginScreen />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={AuthLoginScreen}></Stack.Screen>
+        <Stack.Screen name="Signup" component={AuthSignUpScreen}></Stack.Screen>
+        <Stack.Screen name="MainApp" component={MainApp}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-function AuthLoginScreen() {
+function MainApp() {
+  return (
+    <ThemeProvider>
+      <Drawer.Navigator>
+        <Drawer.Screen
+          name="Home"
+          component={HomeScreenTabedComponent}
+          style={{ backgroundColor: "#222" }}
+        ></Drawer.Screen>
+        <Drawer.Screen
+          name="Calculator"
+          component={CalculatorScreenTabedComponent}
+        ></Drawer.Screen>
+        <Drawer.Screen
+          name="About"
+          component={AboutScreenTabedComponent}
+        ></Drawer.Screen>
+      </Drawer.Navigator>
+    </ThemeProvider>
+  );
+}
+
+function AuthLoginScreen({ navigation }) {
   return (
     <View
       style={{
@@ -94,12 +106,92 @@ function AuthLoginScreen() {
             secureTextEntry={true}
             style={styles.inputField}
           ></TextInput>
-          <Button title="Login" style={{ width: "100%" }} />
+          <Button
+            title="Login"
+            style={{ width: "100%" }}
+            onPress={() => {
+              navigation.navigate("MainApp");
+            }}
+          />
           <View>
             <Text>
               Don't have an account?{" "}
-              <Text style={{ color: "blue", textDecorationLine: "underline" }}>
+              <Text
+                style={{ color: "blue", textDecorationLine: "underline" }}
+                onPress={() => {
+                  navigation.navigate("Signup");
+                }}
+              >
                 Sign Up
+              </Text>
+            </Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+function AuthSignUpScreen({ navigation }) {
+  return (
+    <View
+      style={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-around",
+      }}
+    >
+      <View
+        style={{
+          borderColor: "#00c",
+          borderWidth: 1,
+          borderRadius: 20,
+          shadowColor: "#001",
+          shadowOffset: 4,
+          shadowOpacity: "100%",
+          shadowRadius: 40,
+          width: "75%",
+          height: "70%",
+          marginLeft: "auto",
+          marginRight: "auto",
+        }}
+      >
+        <View
+          style={{
+            padding: 5,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-around",
+            height: "75%",
+          }}
+        >
+          <Text
+            style={{ textAlign: "center", fontWeight: "bold", fontSize: 30 }}
+          >
+            Sign Up
+          </Text>
+          <TextInput
+            placeholder="Username or email"
+            style={styles.inputField}
+          ></TextInput>
+          <TextInput
+            placeholder="Password"
+            secureTextEntry={true}
+            style={styles.inputField}
+          ></TextInput>
+          <Button title="Sign Up" style={{ width: "100%" }} />
+          <View>
+            <Text>
+              Have an account?{" "}
+              <Text
+                style={{ color: "blue", textDecorationLine: "underline" }}
+                onPress={() => {
+                  navigation.navigate("Login");
+                }}
+              >
+                Login
               </Text>
             </Text>
           </View>
