ecf1fa6c23d298b1018a533882a222364dee9149
hirwaNkevin <hirwankevin@gmail.com>
2024-02-24 20:21:17 +0200

    try loading contacts from phone

--- screens/ContactsScreen.js
+++ screens/ContactsScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import {
   View,
   TextInput,
@@ -10,12 +10,45 @@ import {
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { ThemeProvider } from "../components/ThemeProvider";
 import ThemeContext from "../components/ThemeProvider";
+import { Contacts } from "react-native-contacts";
 
 export default ContactsScreen = () => {
+  let contactsList;
+  useEffect(() => {
+    console.log("loading...");
+    loadContacts();
+    console.log("finished");
+  }, []);
+
+  const loadContacts = async () => {
+    try {
+      // Request permission
+      PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_CONTACTS)
+        .then((granted) => {
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            // Get contacts
+            Contacts.getAll((err, contacts) => {
+              if (err) {
+                console.log(err);
+                return;
+              }
+              console.log(contacts);
+            });
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } catch (error) {
+      console.log("" + error);
+      contactsList = "Oops something went wrong";
+    }
+  };
+
   const { theme, updateTheme } = useContext(ThemeContext);
   return (
-    <View style={{ backgroundColor: theme.background }}>
-      <Text>Contacts!</Text>
+    <View style={{ backgroundColor: theme.background, height: "100%" }}>
+      <Text style={{ color: theme.text }}>Contacts</Text>
     </View>
   );
 };
